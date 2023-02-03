f720edef9a465bc951670e5e724cd466f1f71542
rishabhp2019 <55821392+rishabhp2019@users.noreply.github.com>
2023-02-03 13:41:40 -0500

    Add files via upload

---
+++ website/js/main.js
@@ -0,0 +1,29 @@
+// Start of JS functions for the login page
+
+function field_focus(field, email)
+  {
+    if(field.value == email)
+    {
+      field.value = '';
+    }
+  }
+
+  function field_blur(field, email)
+  {
+    if(field.value == '')
+    {
+      field.value = email;
+    }
+  }
+
+//Fade in dashboard box
+$(document).ready(function(){
+    $('.box').hide().fadeIn(1000);
+    });
+
+//Stop click event
+$('a').click(function(event){
+    event.preventDefault(); 
+	});
+
+// End of JS Functions for login page
